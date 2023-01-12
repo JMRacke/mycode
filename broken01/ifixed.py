af4a76d096e1292bae568d7f9768fdc89bd7bad6
JMRacke <joeracke@gmail.com>
2023-01-12 23:57:48 +0000

    challenge 50

--- broken01/ifixed.py
+++ broken01/ifixed.py
@@ -5,21 +5,23 @@
 calc1 = 0.0
 calc2 = 0.0
 operation = ""
-while (calc1 != "q"):
+keepgoing = "y"
+while (keepgoing.lower() == "y"):
     calc1 = input("\nWhat is the first operator? Or, enter q to quit: ")
-    if calc1 == "q":
+    if calc1.lower() == "q":
         break
     calc1 = float(calc1)
     calc2 = input("\nWhat is the second operator? Or, enter q to quit: ")
-    if calc2 == "q":
+    if calc2.lower() == "q":
         break
     calc2 = float(calc2)
-    print("Enter an operation to perform on the two operators (+ or -): ")
-    operation = raw_input()
+    operation = input("Enter an operation to perform on the two operators (+ or -): ")
     if operation == "+":
         print("\n" + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
     elif operation == '-':
         print("\n" + str(calc1) + " - " + str(calc2) + " = " + str(calc1 - calc2))
     else:
         print("\n Not a valid entry. Restarting...")
+    
+    keepgoing = input("use calc again? Y/N ")
 
