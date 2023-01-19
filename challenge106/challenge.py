ea5784c428ad640659848831000a65cc9464fe83
JMRacke <joeracke@gmail.com>
2023-01-19 23:22:15 +0000

    trivia challenges

--- challenge106/challenge.py
+++ challenge106/challenge.py
@@ -1,23 +1,40 @@
 #1/usr/bin/env python3
 
-URL= "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple"
-
 from flask import Flask
 from flask import redirect
 from flask import request
 from flask import render_template
+from flask import url_for
 import requests
 import random
+import html
+
 
 app = Flask(__name__)
-correct = ""
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/trivlogin",methods=["POST"])
+def trivlogin():
+    return redirect(url_for("trivia",name=request.form.get("nm")))
+
+@app.route("/correct")
+def correct():
+    return render_template("correct.html") 
+
+@app.route("/incorrect")
+def incorrect():
+    return render_template("wrong.html") 
 
 @app.route("/trivia")
 def trivia():
-    data = requests.get(URL).json()
+    url= f"https://opentdb.com/api.php?amount=10&category={random.randint(1,31)}&difficulty=medium&type=multiple"
+    data = requests.get(url).json()
     pydata = data['results']
     qdict = pydata.pop(random.randint(0,(len(pydata)-1)))
-    question = qdict['question'] # question to send
+    question = html.unescape(qdict['question']) # question to send
     answers = [qdict['correct_answer']]
     answers.extend(qdict['incorrect_answers'])
     letters = 'ABCD'
@@ -25,7 +42,7 @@ def trivia():
     opts = []
     for i in range(4):
         rand = random.randint(0,(len(answers)-1))
-        options[letters[i]] = answers.pop(rand)
+        options[letters[i]] = html.unescape(answers.pop(rand))
     for let in options:
         opts.append(options.get(let))
     opt1 = opts[0]
@@ -34,16 +51,16 @@ def trivia():
     opt4 = opts[3]
     correct = qdict['correct_answer']
     
-    return render_template("trivia.html",question = question,opt1 = opt1,opt2=opt2,opt3=opt3,opt4=opt4,correct=qdict['correct_answer'])
+    return render_template("trivia.html",name = request.args.get('name'),question = question,opt1 = opt1,opt2=opt2,opt3=opt3,opt4=opt4,correct=qdict['correct_answer'])
 
 @app.route("/submit",methods=["POST"])
 def submit():
     print(request.form.get("choice"))
     print(request.form.get("correct"))
     if request.form.get("choice") == request.form.get("correct"):
-        return render_template("correct.html")
+        return redirect(url_for("correct"))
     else:
-        return render_template("wrong.html")
+        return redirect(url_for("incorrect"))
 
 
 if __name__ == "__main__":
